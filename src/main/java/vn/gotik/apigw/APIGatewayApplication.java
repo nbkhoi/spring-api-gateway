432d26307fef1a462a181f33c362d2e887d23c12
Khoi Nguyen Bao <nbkhoi@tma.com.vn>
2024-11-06 21:31:17 +0700

    init commit

---
+++ src/main/java/vn/gotik/apigw/APIGatewayApplication.java
@@ -0,0 +1,12 @@
+package vn.gotik.apigw;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
